1bc12f97e4ca2df54a56142269d1257c79d904df
MD Mostafa Niloy <mostafa@visernic.com>
2025-11-04 09:02:24 +0600

    Create device-detector.php

---
+++ src/config/device-detector.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Device Detector Configuration
+    |--------------------------------------------------------------------------
+    |
+    | You can define additional configuration options here if needed.
+    |
+    */
+
+    'some_option' => env('DEVICE_DETECTOR_OPTION', true),
+
+];
